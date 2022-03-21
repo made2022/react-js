0efede2cb7aa7178ae78a0df17bc01294da400d0
Madeleyne Toro Sanchez <madeleynetorosanchez@gmail.com>
2022-03-21 21:11:30 +0100

    commit initial

---
+++ src/components/GifGrid.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridltem';
+// import { getGifs } from '../helpers/getGifs';
+
+export const GifGrid = ({ category }) => {
+
+   // const [images, setImages] = useState([]);
+
+   // useEffect( () => {
+   //     getGifs( category )
+   //          .then( setImages );
+   // },  [ category ])
+   
+
+    return (
+        <>
+            <h3> { category } </h3> 
+            {/* <div className="card-grid">   
+                
+               {
+
+                   images.map( img => (
+                       <GifGridItem 
+                          key={ img.id }
+                          {... img }
+
+                        />                 
+                   ))
+                }
+
+            </div> */}       
+        </>
+    )
+}
